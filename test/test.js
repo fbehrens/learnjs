3ed5eea65cb6da288fd40540f9d1784a08a17a60
Frank Behrens <fbehrens@gmail.com>
2022-01-25 22:25:32 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
+
+    describe('more tests', function (){
+        it('case 1',function (){
+            assert.strictEqual(1,1)
+        })
+    })
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
